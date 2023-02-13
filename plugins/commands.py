c2fb8e756aec9b8108fdd696dae43f6c354d0ec8
Zac <91833744+saminsumesh@users.noreply.github.com>
2023-02-13 19:46:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,25 +24,14 @@ from utils import extract_link, get_me_button, get_size, getHerokuDetails
 logger = logging.getLogger(__name__)
 
 user_commands = [
-    "mdisk_api",
-    "shortener_api",
+    "xplinks_api",
     "header",
     "footer",
     "username",
-    "banner_image",
-    "base_site",
     "me",
 ]
 avl_web = [
-    "droplink.co",
-    "gplinks.in",
-    "tnlink.in",
-    "za.gl",
-    "du-link.in",
-    "viplink.in",
-    "shorturllink.in",
-    "shareus.in",
-    "earnspace.in",
+    "xplinks.in"
 ]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
